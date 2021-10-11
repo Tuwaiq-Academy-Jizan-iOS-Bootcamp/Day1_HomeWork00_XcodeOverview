80f5b38cd9606d03e4db55bcd2973dd4a67c409a
Faten Abdullh salem <faten@MacBook-Pro-Faten.local>
2021-10-11 04:32:50 -0700

    add project

---
+++ MyFrisApp/MyFrisApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFrisApp
+//
+//  Created by Faten Abdullh salem on 05/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
