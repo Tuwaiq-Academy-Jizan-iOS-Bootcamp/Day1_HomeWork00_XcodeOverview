1af1e2755eed23d72d2ecc99ff13f37a18efeff3
Ahmad Barqi <ahmadbarqi@MacBook-Pro-Ahmad.local>
2021-10-11 14:59:27 +0300

    done

---
+++ Ahmad/Ahmad Swift/Ahmad Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ahmad Swift
+//
+//  Created by Ahmad Barqi on 05/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
