594ff51e234742c33fa617ab4d3a7868c467a095
ماك بوك <makbwk@MacBook-Pro-D82.local>
2021-10-11 13:59:07 +0300

    this is my first ig App

---
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by ماك بوك on 05/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
