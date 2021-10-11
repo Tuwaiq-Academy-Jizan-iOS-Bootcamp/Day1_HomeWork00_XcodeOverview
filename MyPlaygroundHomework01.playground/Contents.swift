59931bfb627cdded85bcaf6789b7a8d927e30a56
hamad <7amad7arisi@gmail.com>
2021-10-11 22:30:42 +0300

    ...

--- MyPlaygroundHomework01.playground/Contents.swift
+++ MyPlaygroundHomework01.playground/Contents.swift
@@ -1,2 +1,18 @@
-let
+let myName = "Hamad Harisi"
+ var myAge = 24
+ var myCity:String = "Jazan"
+
+ print("Name:", myName)
+ print("Age: ", myAge)
+ print("City:",myCity)
+
+
+ print("\n My Best 3 Hobbies")
+ var myFirstHobby = "play Football"
+ var mySecondHobby = "Watching movies"
+ var myThirdhobby = "Watching Football"
+
+ print("1- ",myFirstHobby)
+ print("2- ",mySecondHobby)
+ print("3- ",myThirdhobby)
 
