46e86f67fbb2242599397d773757b89796457f19
Nasser Aseeri <nasseraseeri@Nassers-MacBook-Air.local>
2021-10-11 14:04:17 +0300

    new project

---
+++ nasser ali project/nasser ali project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nasser ali project
+//
+//  Created by Nasser Aseeri on 05/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
