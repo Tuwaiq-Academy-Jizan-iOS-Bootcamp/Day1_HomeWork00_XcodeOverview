11c6f216e96e943d6861abb48c7c428d8465227b
Afrah Omar <afrahomar@MacBook-Pro-Afrah.local>
2021-10-11 15:41:12 +0300

    add project

---
+++ MyFirstApp/myFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myFirstApp
+//
+//  Created by Afrah Omar on 05/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ myfirst/myfirst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myfirst
+//
+//  Created by Afrah Omar on 05/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
