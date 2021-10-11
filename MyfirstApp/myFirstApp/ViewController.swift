36e8d8e9d2761c93bf8e049f8b9c3fa5ad58bc01
Arwa Alattas <arwaalattas@MacBook-Pro-Arwa.local>
2021-10-11 14:46:34 +0300

    my first App

---
+++ MyfirstApp/myFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myFirstApp
+//
+//  Created by Arwa Alattas on 05/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
