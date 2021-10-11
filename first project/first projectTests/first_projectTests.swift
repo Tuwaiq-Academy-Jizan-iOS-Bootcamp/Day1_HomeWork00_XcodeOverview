2c43f12ea1e87eb5ef5a62e46d93d15ee38ac331
Bushra Barakat <bushrabarakat@MacBook-Pro-Bushra.local>
2021-10-11 14:19:50 +0300

    first project

---
+++ first project/first projectTests/first_projectTests.swift
@@ -0,0 +1,33 @@
+//
+//  first_projectTests.swift
+//  first projectTests
+//
+//  Created by Bushra Barakat on 05/03/1443 AH.
+//
+
+import XCTest
+@testable import first_project
+
+class first_projectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ first project/first projectUITests/first_projectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  first_projectUITestsLaunchTests.swift
+//  first projectUITests
+//
+//  Created by Bushra Barakat on 05/03/1443 AH.
+//
+
+import XCTest
+
+class first_projectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
