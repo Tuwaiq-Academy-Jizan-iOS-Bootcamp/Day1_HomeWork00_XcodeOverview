51b071c4c362b079c176e6d2c5136140499a65d8
Abdulrahman Gazwani <a.g@MacBook-Air-Abdulrahman.local>
2021-10-11 14:16:56 +0300

    done

---
+++ MyAPP/MyAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAPP
+//
+//  Created by Abdulrahman Gazwani on 05/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
