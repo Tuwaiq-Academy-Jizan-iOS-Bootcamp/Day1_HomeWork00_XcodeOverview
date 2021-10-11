3c7d062efc03d78c574fba970e747900ad157b76
Dua'a ageel <duaaageel@MacBook-Pro-Duaa.local>
2021-10-11 14:18:21 +0300

    my project

---
+++ pr5/pr5UITests/pr5UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pr5UITestsLaunchTests.swift
+//  pr5UITests
+//
+//  Created by Dua'a ageel on 05/03/1443 AH.
+//
+
+import XCTest
+
+class pr5UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
