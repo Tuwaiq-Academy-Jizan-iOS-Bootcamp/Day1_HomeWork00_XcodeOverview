657ebc6edc0e6882a1a0971a8e12fc89189e10da
AMALJELI <amaljeli@MacBook-Air-Amal.local>
2021-10-11 14:18:50 +0300

    my frist app

---
+++ my frist app/AMAL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AMAL
+//
+//  Created by Amal Jeli on 05/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
